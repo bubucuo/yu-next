07c427dba999e5d821f541611578c68bce401cb4
bubucuo <276185876@qq.com>
2024-12-06 17:58:28 -0800

    vercel提示

--- src/app/ssr/without-server/page.tsx
+++ src/app/ssr/without-server/page.tsx
@@ -2,6 +2,7 @@ export default async function ServerComponentWithoutServer() {
   const res = await fetch("https://fakestoreapi.com/products/1").then((res) =>
     res.json()
   );
+  // ! 注意下面的这个地址，部署的时候，不要部署到线上。因为线上目前还没有这个地址。除非你有。不然就报错了
   // const res = await fetch("http://localhost:3000").then((res) => res.json());
   return (
     <div>
